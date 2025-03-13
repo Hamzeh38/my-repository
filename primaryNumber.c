69c29abbb71c22097e87d888186998d791efd599
Hamzeh38 <hamzekassan38@gmail.com>
2025-03-13 10:00:01 +0100

    Create a function to check if the input is a primary number

--- primaryNumber.c
+++ primaryNumber.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 
-int main(){
-
-    int number , is_primary = 1 ;
-
-    /*System input*/
-    printf("Enter a number : ");
-    scanf("%d" , &number);
-
-    /*Checking if primary*/
+int primaryChecking(int number){
+    int is_primary = 1;
     if(number <= 1)
-       is_primary = 0;
+        is_primary = 0;
     else 
         for (int i = 2 ; i <= (number / 2) ; i++)
             if( number % i == 0 )
                 is_primary = 0 ;
-                
-    /*Display*/
-    if (is_primary == 1)
-        printf("%d is a primary number" , number);
-    else
-        printf("%d is not a primary number" , number);
+    return is_primary;
+}
 
+int main(){
+
+    int Input;
+
+    /*System input*/
+    printf("Enter a number : ");
+    scanf("%d" , &Input);
+
+    /*Checking if primary*/
+    int check = primaryChecking(Input);
+      
+    /*Display*/
+    if (check == 1)
+        printf("%d is a primary number" , Input);
+    else
+        printf("%d is not a primary number" , Input);
+        
     return 0;
 }
